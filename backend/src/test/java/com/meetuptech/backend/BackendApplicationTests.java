4bbe877354725cce2308cb4d82f464eb9cbc1314
Abbas Yasmina <yasmina.abbas@etu.univ-lyon1.fr>
2026-03-29 05:07:47 +0200

    revert: restore backend to original state before Sonar changes

--- backend/src/test/java/com/meetuptech/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/meetuptech/backend/BackendApplicationTests.java
@@ -2,13 +2,12 @@ package com.meetuptech.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
