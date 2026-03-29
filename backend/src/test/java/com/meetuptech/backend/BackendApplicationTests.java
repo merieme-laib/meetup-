41f697e9faf7c4114171e5f43b2d8020f1329094
Abbas Yasmina <yasmina.abbas@etu.univ-lyon1.fr>
2026-03-29 03:07:03 +0200

    fix: add H2 for tests, JaCoCo coverage, fix CI to run tests

--- backend/src/test/java/com/meetuptech/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/meetuptech/backend/BackendApplicationTests.java
@@ -2,12 +2,21 @@ package com.meetuptech.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "jwt.secret=test_secret_key_very_long_and_secure_for_testing",
+    "jwt.expiration=86400000"
+})
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
